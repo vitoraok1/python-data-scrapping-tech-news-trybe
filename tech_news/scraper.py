80ceae773a04ec0c890ee1161344a91d3a34cfe8
Vitor Aoki <vitoraoki17@gmail.com>
2023-08-31 16:48:36 -0300

    requirement 04 - creating scrape_news function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+import re
 
 
 # Requisito 1
@@ -33,8 +34,31 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = Selector(text=html_content)
+
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    title = selector.css(".entry-title::text").get().strip()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css(".url.fn.n::text").get()
+    reading_time_str = selector.css(".meta-reading-time::text").get()
+    """Método re.findall para pegar apenas os números da string:
+    https://pythonexamples.org/
+    python-regex-extract-find-all-the-numbers-in-string/"""
+    reading_time = int(re.findall("[0-9]+", reading_time_str)[0])
+    summary = selector.css(
+        ".entry-content > p:first-of-type *::text"
+    ).getall()
+    category = selector.css(".category-style .label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": reading_time,
+        "summary": "".join(summary).strip(),
+        "category": category,
+    }
 
 
 # Requisito 5
