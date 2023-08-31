b580ebec0a222c82486b2f88eef9e30b3f10c4c0
Vitor Aoki <vitoraoki17@gmail.com>
2023-08-31 18:35:23 -0300

    requirement 08 - creating search_by_date function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -15,8 +16,20 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    """datetime.strptime para converter string em datetime:
+    https://www.programiz.com/python-programming/datetime/strptime
+    datetime.strftime para converter datetime em string:
+    https://www.programiz.com/python-programming/datetime/strftime"""
+    try:
+        date_format = datetime.strptime(date, "%Y-%m-%d")
+        new_date_format = date_format.strftime("%d/%m/%Y")
+        news = search_news({"timestamp": {"$regex": new_date_format}})
+        tuple_list = []
+        for new in news:
+            tuple_list.append((new["title"], new["url"]))
+        return tuple_list
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
